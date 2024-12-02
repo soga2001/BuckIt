30f730d7d71baed9b227f394772d3322e37e82f2
Suyogya Poudel <soga2058@gmail.com>
2024-12-02 13:56:49 -0700

    Created basic Navbar Component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,34 @@
 import { Component, ViewChild } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
-
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    ButtonModule,
+    Ripple,
+    AvatarModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   animations: [],
 })
 
 export class NavbarComponent {
+
+  constructor(private router: Router) {}
+
+  isLinkActive(url: string): boolean {
+    const queryParamsIndex = this.router.url.indexOf('?');
+    const baseUrl = queryParamsIndex === -1 ? this.router.url : this.router.url.slice(0, queryParamsIndex);
+    return baseUrl === url;
+  }
 }
