ee9e065af95fc9f3aa7cf9bb6cb273f651f7c542
Suyogya Poudel <soga2058@gmail.com>
2024-12-02 14:02:55 -0700

    Created a profile page, and updated routes and app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,33 @@
-import { Routes } from '@angular/router';
+import { Routes, UrlSegment } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginInformationComponent } from './register/login-information/login-information.component';
 import { PersonalInformationComponent } from './register/personal-information/personal-information.component';
 import { UploadAvatarComponent } from './register/upload-avatar/upload-avatar.component';
 import { VerifyAccountComponent } from './register/verify-account/verify-account.component';
-
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        data: {
+            title: "Home"
+        }
+    },
+    {
+        // path: `:username`,
+        component: ProfileComponent,
+        data: {
+            title: "Profile"
+        },
+        matcher: (url) => {
+            if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+              return {consumed: url, posParams: {username: new UrlSegment(url[0].path.slice(1), {})}};
+            }
+            return null;
+          },
+    },
     {
         path: 'login',
         component: LoginComponent,
@@ -38,5 +59,9 @@ export const routes: Routes = [
                 component: VerifyAccountComponent, // another child route component that the router renders
             }
         ]
+    },
+    {
+        path: '**',
+        redirectTo: ''
     }
 ];
